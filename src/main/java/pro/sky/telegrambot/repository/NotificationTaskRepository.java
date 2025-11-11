fff26dfc145515338eea57e73ea287471949a2c1
KaRtOfElIo <164926717+KaRtOfElIo@users.noreply.github.com>
2025-11-11 15:26:50 +0300

    kursovaia
    kursovai

--- src/main/java/pro/sky/telegrambot/repository/NotificationTaskRepository.java
+++ src/main/java/pro/sky/telegrambot/repository/NotificationTaskRepository.java
@@ -8,7 +8,7 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 @Repository
-public interface NotificationTaskRepository extends JpaRepository<NotificationTask, Long> {
+public interface  NotificationTaskRepository extends JpaRepository<NotificationTask, Long> {
 
     List<NotificationTask> findByNotificationDateTime(LocalDateTime notificationDateTime);
 }
